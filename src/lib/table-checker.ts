afe60340384c4265755c230a498b221d481a10f6
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-09-14 22:15:43 +0200

    format

--- src/lib/table-checker.ts
+++ src/lib/table-checker.ts
@@ -2,23 +2,23 @@ import type { Database } from "db0";
 import type { tableNames } from "..";
 
 export class TableChecker {
-  dbClient: Database;
-  tableNames: tableNames;
+	dbClient: Database;
+	tableNames: tableNames;
 
-  constructor(dbClient: Database, tableNames: tableNames) {
-    this.dbClient = dbClient;
-    this.tableNames = tableNames;
-  }
+	constructor(dbClient: Database, tableNames: tableNames) {
+		this.dbClient = dbClient;
+		this.tableNames = tableNames;
+	}
 
-  async checkUserTable(): Promise<boolean> {
-    throw new Error("checkUserTable not implemented");
-  }
+	async checkUserTable(): Promise<boolean> {
+		throw new Error("checkUserTable not implemented");
+	}
 
-  async checkSessionTable(): Promise<boolean> {
-    throw new Error("checkSessionTable not implemented");
-  }
+	async checkSessionTable(): Promise<boolean> {
+		throw new Error("checkSessionTable not implemented");
+	}
 
-  async checkOauthAccountTable(): Promise<boolean> {
-    throw new Error("checkOauthAccountTable not implemented");
-  }
+	async checkOauthAccountTable(): Promise<boolean> {
+		throw new Error("checkOauthAccountTable not implemented");
+	}
 }
